1f4acc8c99465ef72a4448f4328b76efc3697307
vivek210404 <vivek210404@gmail.com>
2025-09-28 17:36:39 +0530

    DB connection

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 
-app.use(cors());
+app.use(cors({ origin: CLIENT_ORIGIN, credentials: true }));
 app.use(express.json());
 
 app.get("/api/test", (req, res) => {
-  res.json("chal rha hai");
+    res.json("chal rha hai");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
 });
